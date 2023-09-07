9058507f9f3839987c6c9f472f7d4e71dfe6a152
harshit kumar <harshit.kumar@esamudaay.com>
2023-09-08 00:49:38 +0530

    fir commit

---
+++ src/main/java/in/succinct/bpp/woocommerce/configuration/AppInstaller.java
@@ -0,0 +1,11 @@
+package in.succinct.bpp.woocommerce.configuration;
+
+import com.venky.swf.configuration.Installer;
+
+public class AppInstaller implements Installer {
+
+	public void install() {
+
+	}
+}
+
